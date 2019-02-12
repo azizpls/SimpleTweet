4b4dfbbdee4bd8db192f06ed2cda14670648f857
azizpls <andrewaziz@mac.com>
2019-02-12 17:12:29 -0500

    Second Commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class User {
@@ -9,8 +10,12 @@ public class User {
     public String screenName;
     public String profileImageUrl;
 
-    public static User fromJson(JSONObject jsonObject) {
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
+        user.name = jsonObject.getString("name");
+        user.uid = jsonObject.getLong("id");
+        user.screenName = jsonObject.getString("screen_name");
+        user.profileImageUrl = jsonObject.getString("profile_image_url_https");
 
         return user;
 
